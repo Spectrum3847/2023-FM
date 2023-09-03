495c097c74b532c17c587d90e87a8b91050de4c9
Daniel-J101 <frostysausage123@gmail.com>
2023-09-03 13:44:47 -0500

    added compatibility 4 conditional commands

--- src/main/java/frc/robot/swerve/commands/DriveToVisionTarget.java
+++ src/main/java/frc/robot/swerve/commands/DriveToVisionTarget.java
@@ -23,8 +23,11 @@ public class DriveToVisionTarget extends PIDCommand {
 
     private static double out = 0;
     private Command alignToTag;
-    /** Creates a new DriveToVisionTarget. */
+    private Command conditionalCommand = null;
+    private double conditionalVerticalSetpoint; 
+    private boolean commandStarted = false;
     /**
+     * Creates a new DriveToVisionTarget.
      * Aligns to a vision target in both X and Y axes (field-oriented). If used for automation
      * purposes, it is best to give it a timeout as a maximum timeout
      *
@@ -52,6 +55,23 @@ public class DriveToVisionTarget extends PIDCommand {
         this.getController().setTolerance(tolerance);
     }
 
+    /**
+     * Creates a new DriveToVisionTarget.
+     * Optionally add a command to run once the robot is a certain distance away from a target using vertical setpoints (ex: lower intake when ~1 meter away from target). The conditional command will end with the drive command if not stopped sooner
+     * 
+     *
+     * @param horizontalOffset adjustable offset in the Y axis in case robot isn't completely
+     *     aligned. Default value should be 0
+     * @param pipeline the pipeline to use for vision {@link VisionConfig}
+     * @param conditionalCommand command or sequence to run once the conditionalVerticalSetpoint has been reached
+     * @param conditionalVerticalSetpoint vertical setpoint to run the conditional command at. (Limelight `ty` value)
+     */
+    public DriveToVisionTarget(double horizontalOffset, int pipeline, Command conditionalCommand, double conditionalVerticalSetpoint) {
+        this(horizontalOffset, pipeline);
+        this.conditionalCommand = conditionalCommand;
+        this.conditionalVerticalSetpoint = conditionalVerticalSetpoint;
+    }
+
     @Override
     public void initialize() {
         super.initialize();
@@ -63,11 +83,24 @@ public class DriveToVisionTarget extends PIDCommand {
     public void execute() {
         super.execute();
         alignToTag.execute();
+        if(conditionalCommand != null) { //TODO: review, maybe <=
+            if(getVerticalOffset() >= conditionalVerticalSetpoint) {
+                if(!commandStarted) {
+                    conditionalCommand.initialize();
+                    commandStarted = true;
+                } else {
+                    conditionalCommand.execute();
+                }
+            }
+        }
     }
 
     @Override
     public void end(boolean interrupted) {
         alignToTag.end(interrupted);
+        if(conditionalCommand != null && commandStarted == true) {
+            conditionalCommand.end(interrupted);
+        }
         Robot.swerve.stop();
     }
 
